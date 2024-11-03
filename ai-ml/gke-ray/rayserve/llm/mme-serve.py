e36f90ccb5db9fffa2cc2d8523634a14c40032f3
Popovine Anton <anton.popovine@gmail.com>
2024-11-03 20:00:40 +0000

    fix gpu mem

--- ai-ml/gke-ray/rayserve/llm/mme-serve.py
+++ ai-ml/gke-ray/rayserve/llm/mme-serve.py
@@ -139,14 +139,14 @@ class MultiModelDeployment:
         return Response(content=response)
 
 multi_model = MultiModelDeployment.bind({
-    "foo": VLLMDeployment.options(ray_actor_options={"num_cpus": 8, "num_gpus": 0.4}).bind(
+    "foo": VLLMDeployment.options(ray_actor_options={"num_cpus": 8, "num_gpus": 0.40}).bind(
         model="google/gemma-2b-it",
         tensor_parallel_size=1,
-        gpu_memory_utilization=0.33
+        gpu_memory_utilization=0.40
     ),
-    "bar": VLLMDeployment.options(ray_actor_options={"num_cpus": 8, "num_gpus": 0.4}).bind(
+    "bar": VLLMDeployment.options(ray_actor_options={"num_cpus": 8, "num_gpus": 0.40}).bind(
         model="google/gemma-2b-it",
         tensor_parallel_size=1,
-        gpu_memory_utilization=0.33
+        gpu_memory_utilization=0.40
     ),
 })
